cfea998786242684004c97a115dc05b8b966af04
sagebeme <eudicas5k@gmail.com>
2022-11-07 00:04:47 +0300

    README update and echomain push

---
+++ echomain.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc,char *argv[])
+{
+	printf("The value at array Zero eq: %s\n",argv[0]);
+	printf("The value at arry one eq: %s\n",argv[1]);
+	return(0);
+}
